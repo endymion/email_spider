7a16f473497e3976d0aaac4739340152895e6823
Ryan Alyn Porter <rap@endymion.com>
2012-05-12 18:54:59 -0400

    Use PStore page storage engine.

--- crawl.rb
+++ crawl.rb
@@ -6,10 +6,10 @@ addresses = []
 
 ARGV.each do |url|
   Anemone.crawl(url) do |anemone|
+    anemone.storage = Anemone::Storage.PStore('pages.pstore')
     anemone.on_every_page do |page|
       addresses << page.body.scan(/[\w\d]+[\w\d.-]@[\w\d.-]+\.\w{2,6}/)
       addresses = addresses.flatten.uniq
-      print '.'
     end
   end
 end
