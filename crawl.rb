b59b01bc1e8cafefca57254198f92c11e6523e38
Ryan Alyn Porter <rap@endymion.com>
2012-05-12 13:57:16 -0400

    Added email address recognition.

--- crawl.rb
+++ crawl.rb
@@ -1,9 +1,18 @@
 require 'anemone'
 
+print 'Scanning'
+
+addresses = []
+
 ARGV.each do |url|
   Anemone.crawl(url) do |anemone|
     anemone.on_every_page do |page|
-        puts page.url
+      addresses << page.body.scan(/[\w\d]+[\w\d.-]@[\w\d.-]+\.\w{2,6}/)
+      addresses = addresses.flatten.uniq
+      print '.'
     end
   end
 end
+
+print "\n"
+puts addresses.flatten.uniq
